8b9af44d72e3acb50b2cd19c8a0e1c1efeba098e
fredericopaulista <fredericopaulista@gmail.com>
2025-12-06 11:34:39 -0300

    refactor: Update navigation links to use `landlord.empresas.index` and `landlord.planos.index` routes.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -126,10 +126,10 @@
                 <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                     {{ __('Dashboard') }}
                 </x-responsive-nav-link>
-                <x-responsive-nav-link :href="route('landlord.tenants.index')" :active="request()->routeIs('landlord.tenants.*')">
+                <x-responsive-nav-link :href="route('landlord.empresas.index')" :active="request()->routeIs('landlord.tenants.*')">
                     {{ __('Vidraçarias') }}
                 </x-responsive-nav-link>
-                <x-responsive-nav-link :href="route('landlord.plans.index')" :active="request()->routeIs('landlord.plans.*')">
+                <x-responsive-nav-link :href="route('landlord.planos.index')" :active="request()->routeIs('landlord.plans.*')">
                     {{ __('Planos') }}
                 </x-responsive-nav-link>
                 <x-responsive-nav-link :href="route('landlord.subscriptions.index')" :active="request()->routeIs('landlord.subscriptions.*')">
